793150d192844e46f3e05be457c196f8fa649dc8
Christoffer Holmberg <mr.skug@gmail.com>
2015-02-19 14:13:45 +0200

    Rudimentary DB population

--- TLKmdb-frontend/Database.cs
+++ TLKmdb-frontend/Database.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace TLKmdb_frontend
 {
@@ -13,7 +16,7 @@ namespace TLKmdb_frontend
         private string baseURL, user, pass, name;
 
         // List of Persons
-        public List<Person> Members = new List<Person>();
+        public List<Person> persons = new List<Person>();
         //public List<ModelType> types = new List<ModelType>();
         public Database()
         {
@@ -21,10 +24,31 @@ namespace TLKmdb_frontend
         }
         public Database(string baseURL, string user, string pass, string name)
         {
+            // Set credentials
             this.baseURL = baseURL;
             this.user = user;
             this.pass = pass;
             this.name = name;
+
+            // populate DB
+            
+        }
+
+        public void populateDB()
+        {
+            Console.WriteLine(baseURL + "/persons/?format=json" + " " + user + pass + "" + "GET");
+            Request responseJson = new Request(baseURL + "/persons", user, pass, "", "GET");
+
+            JArray responseArray = JArray.Parse(responseJson.response);
+
+            List<JToken> tokens = responseArray.Children().ToList();
+
+            foreach (JToken token in tokens)
+            {
+                string personJson = token.ToString();
+                Person person = JsonConvert.DeserializeObject<Person>(personJson);
+                persons.Add(person);
+            }
         }
     }
 }
